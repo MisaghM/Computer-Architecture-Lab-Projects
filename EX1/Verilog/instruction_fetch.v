386e6d50a27bac6078e723cfb00c16e2167ab00d
Pasha Barahimi <pashabarahimi@gmail.com>
2023-02-28 18:54:56 +0330

    Add EX1 IF stage

--- EX1/Verilog/instruction_fetch.v
+++ EX1/Verilog/instruction_fetch.v
@@ -1,3 +1,35 @@
-module InstructionFetch();
+module InstructionFetch(
+    input clk, rst,
+    input branchTaken, freeze,
+    input [31:0] branchAddress,
+    output [31:0] pc, instruction
+);
+    wire [31:0] pcRegIn, pcRegOut, pcAdderOut;
 
+    Register #(32) pcReg(
+        .clk(clk),
+        .rst(rst),
+        .in(pcRegIn),
+        .ld(~freeze),
+        .clr(1'b0),
+        .out(pcRegOut)
+    );
+
+    Adder #(32) pcAdder(
+        .a(pcRegOut),
+        .b(32'd4),
+        .out(pcAdderOut)
+    );
+
+    Mux2To1 #(32) pcMux(
+        .a(pcAdderOut),
+        .b(branchAddress),
+        .sel(branchTaken),
+        .out(pcRegIn)
+    );
+
+    InstructionMemory instMem(
+        .pc(pcRegOut),
+        .inst(instruction)
+    );
 endmodule

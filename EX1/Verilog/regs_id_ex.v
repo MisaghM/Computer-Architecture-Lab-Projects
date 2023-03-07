b9a58de62018f74d710948c60fcf2bcdb67039ed
Pasha Barahimi <pashabarahimi@gmail.com>
2023-03-07 12:02:59 +0330

    Add pcReg to register blocks

--- EX1/Verilog/regs_id_ex.v
+++ EX1/Verilog/regs_id_ex.v
@@ -1,3 +1,11 @@
-module RegsIdEx();
-
+module RegsIdEx(
+    input clk, rst,
+    input [31:0] pcIn,
+    output [31:0] pcOut
+);
+    Register #(32) pcReg(
+        .clk(clk), .rst(rst),
+        .in(pcIn), .ld(1'b1), .clr(1'b0),
+        .out(pcOut)
+    );
 endmodule
--- EX1/Verilog/regs_if_id.v
+++ EX1/Verilog/regs_if_id.v
@@ -2,7 +2,11 @@ module RegsIfId(
     input clk, rst,
     input freeze, flush,
     input [31:0] pcIn, instructionIn,
-    output reg [31:0] pcOut, instructionOut
+    output [31:0] pcOut, instructionOut
 );
-
+    Register #(32) pcReg(
+        .clk(clk), .rst(rst | flush),
+        .in(pcIn), .ld(~freeze), .clr(1'b0),
+        .out(pcOut)
+    );
 endmodule
